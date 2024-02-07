616580d705e6a8a6349fd3d041d32cde9070cba1
suresh <mailsuresh@gmail.com>
2024-02-07 17:32:16 +0530

    first commit for catalogue-deploy

---
+++ terraform/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "sbucket1-remote-state"
+key    = "catalogue-deploy"
+region = "us-east-1"
+dynamodb_table = "sbucket1-locking"
---
+++ terraform/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "sbucket1-remote-state"
+key    = "catalogue-deploy-prod"
+region = "us-east-1"
+dynamodb_table = "sbucket1-locking"
